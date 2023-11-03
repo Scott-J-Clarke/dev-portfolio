031662843009cad3efa3af8742c89a19dc3f319f
Vivid Quill <sjdoublec@gmail.com>
2023-11-03 11:41:32 -0500

    Added 'Resume' section to nav links.

---
+++ src/pages/ResumePage.jsx
@@ -0,0 +1,9 @@
+export default function ResumePage() {
+    return (
+        <div className="container mx-auto px-4">
+            <p>
+                Here is my resume!
+            </p>
+        </div>
+    );
+}
