a1b5ae03afbeafb627a669ed59d320b13c4dea35
Vivid Quill <sjdoublec@gmail.com>
2023-11-08 09:54:55 -0600

    Seem to have properly imported Tailwind for styling.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-// Do I need to import Tailwind for styling? Will it style every page from here?
+import './index.css';
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
