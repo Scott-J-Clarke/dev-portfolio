dbc6c3593b9482872d13f195070569129de9ee68
Vivid Quill <sjdoublec@gmail.com>
2023-11-13 16:08:26 -0600

    Sufficient styling for ResumePage.

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,52 +1,53 @@
 import scottResume from '../public/scott-resume-for-portfolio.pdf';
 import { BsCloudDownload } from 'react-icons/bs';
 
-// All the styling below is done in Bootstrap (not Tailwind!):
 function Resume() {
     return (
         <div>
-            <section className='download-resume'>
-                <div className='flex flex-row'> {/* Tailwind syntax: 'flex flex-row' */}
-                    <h2 className='border-2'>My Resume</h2>
-                </div>
-
+            <section className='bg-gray-500 text-white m-5 mx-64 p-4 text-center'>
                 <div className='flex flex-row'>
-                    <div className='download-info'>
-                        <div className='download-text'>
-                            Download my resume <br/>
-                            <a href={scottResume} className='download-logo' download>
-                                <BsCloudDownload />
+                    <div>
+                        <div className='flex flex-row text-white'>
+                            <a href={scottResume} className='flex items-center'>
+                                Download my resume 
+                                <BsCloudDownload className='ml-2' />
                             </a>
                         </div>
                     </div>
                 </div>
             </section>
 
-            <section id='home-page-body' className='resume-body'>
-                <div className='article column1'>
-                    <p className='column-title'>Front-End Tech</p>
-                    <p className='column-text'>
-                        <ul>
-                            <li>React</li>
-                            <li>HTML</li>
-                            <li>JavaScript</li>
-                            <li>Tailwind</li>
-                            <li>CSS</li>
-                        </ul>
-                    </p>
-                </div>
-
-                <div className='article column2'>
-                    <p className='column-title'>Back-End Tech</p>
-                    <p className='column-text'>
-                        <ul>
-                            <li>Node</li>
-                            <li>MongoDB</li>
-                            <li>GraphQL</li>
-                            <li>MySQL</li>
-                            <li>Express</li>
-                        </ul>
-                    </p>
+            <section className='bg-gray-500 text-white m-5 mx-64 p-4 text-center'>
+                <div className='flex flex-wrap'>
+                    <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4'>
+                        <div className='article'>
+                            <p className='text-2xl font-semibold mb-4'>Front-End Knowledge</p>
+                            <p className='list-disc pl-4'>
+                                <ul>
+                                    <li>React</li>
+                                    <li>HTML</li>
+                                    <li>JavaScript</li>
+                                    <li>Tailwind</li>
+                                    <li>CSS</li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                
+                    <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4'>
+                        <div className='article'>
+                            <p className='text-2xl font-semibold mb-4'>Back-End Knowledge</p>
+                            <p className='list-disc pl-4'>
+                                <ul>
+                                    <li>Node</li>
+                                    <li>MongoDB</li>
+                                    <li>GraphQL</li>
+                                    <li>MySQL</li>
+                                    <li>Express</li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
